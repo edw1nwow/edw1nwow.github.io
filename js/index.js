848b304d06b7ffe9474e28f5b36c536616269173
edw1nwow <best254@ukr.net>
2018-04-16 21:11:29 +0300

    q

--- js/index.js
+++ js/index.js
@@ -44,13 +44,7 @@ function createLayout() {
     }
 }
 
-
 const checker = document.querySelector(".checkBox");
-
-
-
-
-
 
 const playSound = note => {
     const audio = document.querySelector(`audio[data-note=${note}]`);
@@ -58,17 +52,6 @@ const playSound = note => {
     audio.play();
 };
 
-
-
-
-function recall() {
-    if (checker.checked == true) {
-        playSound;
-        alert('on')
-    } else {
-        alert("Sound off")
-    }
-}
 
 const buttons = Array.from(document.querySelectorAll("button"));
 const temp = document.querySelector(".output")
@@ -83,12 +66,27 @@ function callback() {
                 if (i == 0) {
                     const findAllClass = document.querySelectorAll(".toprow");
                     findAllClass[j].className = "toprow button keyboard__btn--active";
+
+                    if (checker.checked == true) {
+                        let note = findAllClass[j].getAttribute('data-note');
+                        playSound(note);
+                    }
+
+
                 } else if (i == 1) {
                     const findAllClass = document.querySelectorAll(".middlerow");
                     findAllClass[j].className = "middlerow button keyboard__btn--active";
+                    if (checker.checked == true) {
+                        let note = findAllClass[j].getAttribute('data-note');
+                        playSound(note);
+                    }
                 } else {
                     const findAllClass = document.querySelectorAll(".bottomrow");
                     findAllClass[j].className = "bottomrow button keyboard__btn--active";
+                    if (checker.checked == true) {
+                        let note = findAllClass[j].getAttribute('data-note');
+                        playSound(note);
+                    }
                 }
             }
         }
@@ -101,9 +99,5 @@ function callOut() {
     findClassToRemove.classList.remove("keyboard__btn--active");
 }
 
-
-
-
 window.addEventListener("keydown", callback);
 createLayout();
-checker.addEventListener("click", recall);
